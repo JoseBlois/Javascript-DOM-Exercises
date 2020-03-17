f499eb36b4659f2099c8e13ab60bd4ac96cc8969
jose blois <joseblois3@gmail.com>
2020-03-16 23:42:32 -0300

    Just separating every exercise by comments

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+// Exercise number 1
 var paragraphs = document.getElementsByTagName('p');
 console.log(paragraphs[0]);
+// Exercise number 2
 var nav_bar = document.getElementById('nav-bar');
 console.log(nav_bar);
+// Exercise number 3
 var blacked = document.getElementsByClassName('black');
 for (let i = 0; i < blacked.length; i++) {
     element = blacked[i];
+    element.style.display = 'inline-block';
     element.style.width = '100px';
     element.style.height = '100px';
     element.style.background = '#00FF00';
 }
+// Exercise number 4
 var footer = document.getElementById('footer');
 var black1 = footer.getElementsByClassName('black');
 for (let i = 0; i < black1.length; i++) {
     element = black1[i];
-    element.style.display = 'inline-block';
     element.style.width = '100px';
     element.style.height = '100px';
     element.style.background = '#00AA00';
 }
+// Exercise number 5
 var Showids = function(array){
     for (let i = 0; i < array.length; i++) {
         var element = array[i];
@@ -26,6 +31,7 @@ var Showids = function(array){
 }
 var divs = document.getElementsByTagName('div');
 Showids(divs);
+// Exercise number 6
 var Showsrc = function(array){
     for (let i = 0; i < array.length; i++) {
         var element = array[i];
@@ -52,7 +58,7 @@ for (let idx = 0; idx < img_containers.length; idx++) {
  console.table($paragraphs);
  for (let _idx = 0; _idx < $paragraphs.length; _idx++) {
     var button = document.createElement('button');
-    var content = document.createTextNode('button');
+    var content = document.createTextNode('button '+(_idx+1));
     button.appendChild(content);
      var aux = $paragraphs[_idx];
      aux.appendChild(button);
